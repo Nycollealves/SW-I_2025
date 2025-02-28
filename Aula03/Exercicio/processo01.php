b9edc1be2fa271d6875fd8d05f89b1324754cb1e
Nycollealves <161628270+Nycollealves@users.noreply.github.com>
2025-02-28 16:59:54 -0300

    Add files via upload

---
+++ Aula03/Exercicio/processo01.php
@@ -0,0 +1,10 @@
+<?php
+    $nome = $_POST['cxnome'];
+    
+
+    $mensagem = $_POST['cxmensagem'];
+
+    echo "Bem-vindo, " ,$nome, " ,a sua mensagem é: " ,$mensagem, " ,mas a minha é as boas novas";
+
+
+?>
