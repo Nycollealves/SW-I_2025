e74921d73c0e84afdd520123d0f54ae1a47e42c9
Nycollealves <nycollealves00@gmail.com>
2025-02-28 09:16:27 -0300

    exercicios prontos

---
+++ Exercicios/exercicio1.php
@@ -0,0 +1,22 @@
+<?php
+    $nome = "Tyrone";
+    $idade = 18;
+    $salario = 100;
+
+    echo "Olá! Qual o seu nome?";
+    <br>;
+    echo $nome;
+    <br>
+    echo "Oi".$nome. "! Quantos anos você tem?";
+    <br>
+    echo $idade;
+    <br>
+    echo "Então você tem" .$idade. "!!!";
+    <br>
+    echo "Quanto você ganha" .$nome. "?";
+    <br>;
+    echo $valor;   
+    <br>;
+    echo $valor."! Eu espero que seja por hora e não por ano!!!!rsrsrs....";
+
+?>
---
+++ Exercicios/exercicio3.php
@@ -0,0 +1,22 @@
+<?php
+    $num1 = 70;
+    $num2 = 7;
+
+    $ad = $num1 + $num2;
+    echo "Sua adição resultou em: ".$ad;
+
+
+    $sub = $num1 - $num2;
+    echo "Sua subtração resultou em: ".$sub;
+
+
+    $div = $num1 / $num2;
+    echo "Sua divisão resultou em: ".$div;
+
+
+    $mult = $num1 * $num2;
+    echo "Sua multiplicação resultou em: ".$mult;
+
+
+    $rest = $num1 % $num2;
+    echo "O resto é: ".$rest;
